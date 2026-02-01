a18a2f953bcaff0d0bcb08e0173828c77bfc65ea
Parag Jyoti Nath <paragjnath@gmail.com>
2026-02-01 16:12:13 +0530

    fixes

--- src/constants/api-endpoints.ts
+++ src/constants/api-endpoints.ts
@@ -1,4 +1,4 @@
-export const BASE_URL = 'http://game.crayfish-musical.ts.net:8000';
+export const BASE_URL = 'https://game.crayfish-musical.ts.net';
 
 export const AUTH_LOGIN_API = 'api/v1/auth/login';
 export const AUTH_SIGNUP_API = 'api/v1/auth/signup';
