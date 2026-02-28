d98a09df41ceccc0a6ae4ee26efea5a6207f9b9c
Parag Jyoti Nath <paragjnath@gmail.com>
2026-02-28 14:20:23 +0530

    fixed base url

--- src/constants/api-endpoints.ts
+++ src/constants/api-endpoints.ts
@@ -1,4 +1,4 @@
-export const BASE_URL = process.env.API_BASE_URL;
+export const BASE_URL = process.env.REACT_APP_API_BASE_URL;
 
 export const AUTH_LOGIN_API = 'api/v1/auth/login';
 export const AUTH_SIGNUP_API = 'api/v1/auth/signup';
