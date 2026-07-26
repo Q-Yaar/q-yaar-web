5c99717a159cff04f3a0c2b69c166508798d1caf
Parag Jyoti Nath <paragjnath@gmail.com>
2026-07-26 20:33:24 +0530

    fix(auth): clear all RTK Query cache and Redux state on logout

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -2,18 +2,30 @@ import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import type { UnknownAction } from "@reduxjs/toolkit";
 import { api } from "../apis/api";
 import { AUTH_MODULE, SERVER_MODULE } from "../constants/modules";
-import authReducer, { AuthState } from "./auth-reducer";
+import authReducer, { AuthState, clearToken } from "./auth-reducer";
+import { storage, STORAGE_KEYS } from "../utils/storage";
 
 export type GlobalState = {
   [AUTH_MODULE]: AuthState;
   [SERVER_MODULE]: ReturnType<typeof api.reducer>;
 };
 
+const appReducer = combineReducers({
+  [AUTH_MODULE]: authReducer,
+  [SERVER_MODULE]: api.reducer,
+});
+
+const rootReducer = (state: GlobalState | undefined, action: UnknownAction) => {
+  if (clearToken.match(action)) {
+    storage.remove(STORAGE_KEYS.AUTH);
+    // Passing undefined resets all reducers (including RTK Query api.reducer) to fresh initial state
+    state = undefined;
+  }
+  return appReducer(state, action);
+};
+
 export const store = configureStore<GlobalState, UnknownAction>({
-  reducer: combineReducers({
-    [AUTH_MODULE]: authReducer,
-    [SERVER_MODULE]: api.reducer,
-  }),
+  reducer: rootReducer as any,
   middleware: (getDefaultMiddleware: any) =>
     getDefaultMiddleware({ serializableCheck: false }).concat(api.middleware),
 });
